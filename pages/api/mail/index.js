0aab1e5dd61509df87d8b7b44349c90fc40b4139
Guilherme Hammes <gffhammes@gmail.com>
2022-05-05 12:13:34 -0300

    admin mail added to confirmation mail

--- pages/api/mail/index.js
+++ pages/api/mail/index.js
@@ -17,6 +17,7 @@ export default async function handler(req, res) {
 
   const data = {
     to: body.userData.email,
+    cc: 'a.d.sanches@gmail.com',
     from: 'a.d.sanches@gmail.com',
     subject: 'Confirmação | Cotas reservadas',
     text: message,
